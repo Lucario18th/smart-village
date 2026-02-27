f01d3f1c76d8b1ce9e174acb49152a66bd4cb456
Manuel <manuelkessler2409@gmail.com>
2026-02-27 13:38:20 +0100

    Kartenimplementation aufgeräumt

--- app/SmartVillageApp/composeApp/src/commonMain/kotlin/de/tif23/studienarbeit/viewmodel/MainViewModel.kt
+++ app/SmartVillageApp/composeApp/src/commonMain/kotlin/de/tif23/studienarbeit/viewmodel/MainViewModel.kt
@@ -9,46 +9,38 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import de.tif23.studienarbeit.provider.makeOsmTileStreamProvider
+import de.tif23.studienarbeit.util.latToY
+import de.tif23.studienarbeit.util.lonToX
+import de.tif23.studienarbeit.viewmodel.constants.LOERRACH_LAT
+import de.tif23.studienarbeit.viewmodel.constants.LOERRACH_LON
 import org.jetbrains.compose.resources.painterResource
 import ovh.plrapps.mapcompose.api.addCallout
 import ovh.plrapps.mapcompose.api.addLayer
 import ovh.plrapps.mapcompose.api.addMarker
-import ovh.plrapps.mapcompose.api.enableRotation
-import ovh.plrapps.mapcompose.api.onCalloutClick
 import ovh.plrapps.mapcompose.api.onMarkerClick
-import ovh.plrapps.mapcompose.api.removeCallout
 import ovh.plrapps.mapcompose.api.scale
-import ovh.plrapps.mapcompose.core.TileStreamProvider
 import ovh.plrapps.mapcompose.ui.layout.Forced
 import ovh.plrapps.mapcompose.ui.state.MapState
 import smartvillageapp.composeapp.generated.resources.Res
 import smartvillageapp.composeapp.generated.resources.add_location
-import smartvillageapp.composeapp.generated.resources.settings
 import kotlin.math.PI
 import kotlin.math.ln
 import kotlin.math.pow
 import kotlin.math.tan
 
 class MainViewModel: ViewModel() {
-    val tileStreamProvider = makeOsmTileStreamProvider()
+    private val tileStreamProvider = makeOsmTileStreamProvider()
     private val maxLevel = 16
     private val minLevel = 12
     private val mapSize = mapSizeAtLevel(maxLevel, tileSize = 256)
 
-    val lonCoordinate = 7.66707
-    val latCoordinate = 47.61379
-    val latRad = latCoordinate * PI / 180
-    val lon = (lonCoordinate + 180) / 360
-    val lat = (1 - ln(tan(PI / 4 + latRad / 2)) / PI) / 2
-
-    val state = MapState(levelCount = maxLevel + 1, mapSize, mapSize, workerCount = 16) {
+    val mapState = MapState(levelCount = maxLevel + 1, mapSize, mapSize, workerCount = 16) {
         minimumScaleMode(Forced(1 / 2.0.pow(maxLevel - minLevel)))
-        scroll(lon, lat)  // Paris
+        scroll(lonToX(LOERRACH_LON), latToY(LOERRACH_LAT))  // Paris
     }.apply {
         addLayer(tileStreamProvider)
         addMarker("altglas1", lonToX(7.6588259), latToY(47.6158539)) {
@@ -88,14 +80,5 @@ class MainViewModel: ViewModel() {
 
     private fun mapSizeAtLevel(wmtsLevel: Int, tileSize: Int): Int {
         return tileSize * 2.0.pow(wmtsLevel).toInt()
-    }
-
-    private fun latToY(lat: Double): Double {
-        val latRad = lat * PI / 180
-        return (1 - ln(tan(PI / 4 + latRad / 2)) / PI) / 2
-    }
-
-    private fun lonToX(lon: Double): Double {
-        return (lon + 180) / 360
     }
 }
